0c3aca20dad11a12b7a1de0dcb6f8f08c0df80b3
Mars.D <145869849+marsxdd@users.noreply.github.com>
2023-12-21 17:41:32 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,26 @@
+#main
+
+import requests
+
+def get_weather(city, country):
+    city = city_entry.get()
+    country = country_entry.get()
+    api_key = "#USE YOUR OWN API KEY!!"
+    base_url = "https://api.openweathermap.org/data/2.5/weather"
+    params = {"q": city, "appid":api_key, "units":"metric"}
+
+    response = requests.get(base_url, params=params)
+    weather_data = response.json()
+
+    if weather_data["cod"]== "404":
+        return "City Not Found!, Please Try Again"
+
+    temperature = weather_data["main"] ["temp"]
+    description = weather_data["weather"][0]["description"]
+
+    return f"The temperature in {city}, {country} is {temperature}°C with {description}"
+
+city = ("Enter City:")
+country = ("Enter country:")
+weather_info = get_weather(city, country)
+print(weather_info)
